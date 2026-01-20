34b7a22f829daaaae20af0ae4ea056711665a149
KrithiAS10 <krithisonu.a.s@gmail.com>
2026-01-20 12:14:09 +0530

    Made changes n task2.java

--- task1.java
+++ task1.java
@@ -1,16 +1,16 @@
 
 import java.util.Scanner;
 
-public class task1 {
+public class Task1 {
 
     public static void main(String[] args) {
         System.out.println("WELCOME TO NUMBER GUESSING GAME \n Conditions: \n 1.There are three rounds \n 2.You can guess 5 times in each attempt \n 3.The score based on number of your attempts to guess the right number");
-        task1 t1 = new task1();
+        Task1 t1 = new Task1();
         t1.attempt();
         t1.round();
     }
 
-    private static int rollDice() {
+    public int rollDice() {
         double randomno = Math.random();
         randomno = randomno * 100;
         randomno = randomno + 1;
@@ -25,6 +25,7 @@ public class task1 {
         for (int i = 0; i < 5; i++) {
             System.out.println("Enter a number:");
             int x = sc.nextInt();
+            count = count + 1;
             if (x < roll) {
                 System.out.println("Lesser than the random number");
             } else if (x > roll) {
@@ -34,17 +35,19 @@ public class task1 {
                 break;
             }
         }
-        count++;
-        System.out.println("Score=" + ((5 - count) / 5) * 100 + "%");
+        System.out.println("Score=" + (((double) (5 - count) / 5) * 100) + "%");
+        if (count == 5) {
+            System.out.println("The Random Number is: " + roll);
+        }
     }
 
     public void round() {
-        for (int i = 0; i < 2; i++) {
+        for (int i = 1; i < 3; i++) {
             System.out.println("If you want to continue the attempt to round " + (i + 1) + " y/n:");
             Scanner sc = new Scanner(System.in);
             String ques = sc.nextLine();
             if (ques.equals("y")) {
-                System.out.println("WELCOME TO ROUND: 2");
+                System.out.println("WELCOME TO ROUND: " + (i + 1));
                 attempt();
             } else {
                 break;
