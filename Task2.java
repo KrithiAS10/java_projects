e83484f9a8fd6e298c2955b7de87ca61ed66130d
KrithiAS10 <krithisonu.a.s@gmail.com>
2026-01-20 16:18:24 +0530

    Made changes in Task2.java

--- Task2.java
+++ Task2.java
@@ -3,32 +3,35 @@ import java.util.Scanner;
 
 public class Task2 {
 
+    static Scanner sc = new Scanner(System.in);
     public int[] sub = new int[8];
     public static String[] subName = {"SEPA", "CN", "TOC", "AI", "WEB", "MiniPro", "RE", "EVS"};
-    public int total = 0;
+    public int total;
     public double avg = 0;
     public String g;
 
     public void inputMark() {
-
-        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the marks in following subject:");
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < sub.length; i++) {
             System.out.print(subName[i] + ":");
             sub[i] = sc.nextInt();
+            if (sub[i] < 0 || sub[i] > 100) {
+                System.out.println("Invalid marks!...Enter again.");
+                i--;
+            }
             System.out.print("\n");
         }
     }
 
     public int totalMark() {
-        for (int i = 0; i < 8; i++) {
-            System.out.println(subName[i] + ":" + sub[i]);
+        total = 0;
+        for (int i = 0; i < sub.length; i++) {
             total += sub[i];
         }
         return total;
     }
 
-    public void avrgMark() {
+    public void calculatePercentage() {
         avg = (double) ((double) total / 800) * 100;
         System.out.println((int) avg + "%");
     }
@@ -36,16 +39,16 @@ public class Task2 {
     public void grade() {
         if (avg >= 90) {
             System.out.println("Grade=A+");
-        } else if (avg >= 80 && avg < 90) {
+        } else if (avg >= 80) {
             System.out.println("Grade=A");
         } else if (avg >= 70) {
             System.out.println("Grade=B+");
-        } else if (avg >= 60 && avg < 70) {
-            System.out.println("Grade=B+");
+        } else if (avg >= 60) {
+            System.out.println("Grade=B");
         } else if (avg >= 50) {
-            System.out.println("Grade=B+");
-        } else if (avg >= 40 && avg < 50) {
-            System.out.println("Grade=B+");
+            System.out.println("Grade=C+");
+        } else if (avg >= 40) {
+            System.out.println("Grade=C");
         } else {
             System.out.println("Fail");
         }
@@ -55,7 +58,7 @@ public class Task2 {
         Task2 t2 = new Task2();
         t2.inputMark();
         System.out.println("Total Marks: " + t2.totalMark());
-        t2.avrgMark();
+        t2.calculatePercentage();
         t2.grade();
     }
 }
